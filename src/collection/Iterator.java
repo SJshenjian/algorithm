d85c535ddb834e2bb7f9020f480277e924242831
SJsunshine <shenjian66666688@sina.com>
2017-03-26 22:47:52 +0800

    Collection redo

---
+++ src/collection/Iterator.java
@@ -0,0 +1,5 @@
+package collection;
+public  interface Iterator{
+	public boolean hasNext();
+	public Object next();
+}
---
+++ src/collection/test/BinarySearchTreeTest.java
@@ -0,0 +1,23 @@
+package collection.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import collection.BinaryTreeNode;
+
+public class BinarySearchTreeTest {
+
+	private BinaryTreeNode root=new BinaryTreeNode(5);
+	
+	@Test
+	public void testInsert(){
+		root.insert(2);
+		root.insert(2);
+		root.insert(7);
+		root.insert(1);
+		root.insert(4);
+		root.insert(3);
+		assertEquals(3,root.getLeft().getRight().getLeft().getData());
+	}
+}
