d01c5abe6be6a46d9fd00213c213d6ccf779bdea
SJsunshine <shenjian66666688@sina.com>
2017-03-31 10:59:23 +0800

    rename

--- src/binaryTree/BinaryTreeNode.java
+++ src/binaryTree/BinaryTreeNode.java
@@ -1,4 +1,4 @@
-package BinaryTree;
+package binaryTree;
 
 import static org.junit.Assert.assertEquals;
 
--- src/graph/DFSWithStack.java
+++ src/graph/DFSWithStack.java
@@ -1,4 +1,4 @@
-package Graph;
+package graph;
 
 import java.util.Scanner;
 import java.util.Stack;
