557fb9140d7d410c79a4e08cac1aa0da6bbd03a8
shenjian <sjshenjian@outlook.com>
2019-07-21 09:19:41 +0800

    新增希尔排序

--- base/src/main/java/sort/InsertSort.java
+++ base/src/main/java/sort/InsertSort.java
@@ -1,5 +1,7 @@
 package sort;
 
+import com.sun.istack.internal.NotNull;
+
 import java.util.Arrays;
 
 /**
@@ -9,6 +11,10 @@ import java.util.Arrays;
  * 对于数组 {3, 5, 2, 4, 1}，它具有以下逆序：(3, 2), (3, 1), (5, 2), (5, 4), (5, 1), (2, 1), (4, 1)，
  * 插入排序每次只能交换相邻元素，令逆序数量减少 1，因此插入排序需要交换的次数为逆序数量
  *
+ * 平均情况下插入排序需要N^2/4次比较与N^2/4次交换
+ * 最坏的情况下，需要N^2/2次比较与N^2/2次交换，即数组是倒叙的
+ * 最好的情况下需要N-1次比较和0次交换，即数组是有序的
+ *
  * @author Jian Shen
  * @version V1.0.0
  * @date 2019/7/20
@@ -16,7 +22,7 @@ import java.util.Arrays;
 public class InsertSort<T extends Comparable<T>> extends Sort<T> {
 
     @Override
-    public void sort(T[] array) {
+    public void sort(@NotNull  T[] array) {
         int length = array.length;
         for (int i = 1; i < length; i++) {
             for (int j = i; j > 0; j--) {
@@ -29,7 +35,7 @@ public class InsertSort<T extends Comparable<T>> extends Sort<T> {
 
     public static void main(String[] args) {
         Sort sort = new InsertSort();
-        Integer[] array = new Integer[]{3, 5, 2, 4, 1, 1};
+        Integer[] array = new Integer[]{3, 3, 5, 2, 4, 1, 1};
         sort.sort(array);
         System.out.println(Arrays.asList(array));
     }
