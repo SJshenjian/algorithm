63714684e550bb7c13f6195dd848e4a8e8859d44
shenjian <sjshenjian@outlook.com>
2019-10-13 11:38:14 +0800

    新增埃拉托斯特尼筛法筛选素数

---
+++ base/src/main/java/math/SieveOfEratosthenes.java
@@ -0,0 +1,42 @@
+package math;
+
+/**
+ * 素数筛选:埃拉托斯特尼筛法
+ *
+ * @author Jian Shen
+ * @version V1.0.0
+ * @date 2019/10/13
+ */
+public class SieveOfEratosthenes {
+
+    public boolean[] sieve(int max) {
+        boolean[] flags = new boolean[max + 1];
+        for (int i = 2; i < flags.length; i++) {
+            flags[i] = true;
+        }
+        int prime = 2;
+        while (prime <= Math.sqrt(max)) {
+            crossOff(flags, prime);
+            prime = getNextPrime(flags, prime);
+        }
+        return flags;
+    }
+
+    private int getNextPrime(boolean[] flags, int prime) {
+        int next = prime + 1;
+        while (next < flags.length && !flags[next]) {
+            next++;
+        }
+        return next;
+    }
+
+    private void crossOff(boolean[] flags, int prime) {
+        for (int i = prime * prime; i < flags.length; i += prime) {
+            flags[i] = false;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new SieveOfEratosthenes().sieve(50));
+    }
+}
